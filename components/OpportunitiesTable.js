8e81b8f62c46c81aeeb1983c0941d1bf3c0499af
ifoxwitu329 <ifoxwitu@icloud.com>
2026-02-18 11:25:29 -0500

    Create OpportunitiesTable component

---
+++ components/OpportunitiesTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OpportunitiesTable = () => {
+  return (
+    <div>
+      <h1>Opportunities Table</h1>
+      {/* Add your table implementation here */}
+    </div>
+  );
+};
+
+export default OpportunitiesTable;
