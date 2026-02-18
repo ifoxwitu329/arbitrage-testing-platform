b866b6f36cad85e5ef733151c5be1b13febeb8ca
ifoxwitu329 <ifoxwitu@icloud.com>
2026-02-18 11:24:54 -0500

    Create PriceChart component

---
+++ components/PriceChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PriceChart = () => {
+    // This is a placeholder for the price chart component
+    return (
+        <div>
+            <h2>Price Chart</h2>
+            {/* Chart implementation will go here */}
+        </div>
+    );
+};
+
+export default PriceChart;
